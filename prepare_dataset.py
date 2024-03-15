5b015b9efb50532a04ef129b2be98da0a30f41ae
Yaroslav Khutornoy <morse1995@mail.ru>
2024-03-15 16:10:29 +0300

    Add val/train splitting

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -32,24 +32,32 @@ def create_multiclass_label(cloud: np.ndarray, fog: np.ndarray):
     return label
 
 
-def prepare_dataset(src_root: str,
+def prepare_dataset(src_path: str,
                     dst_root: str,
                     tile_size: int,
                     resize_factor: float = 1.0,
-                    skip_empty: bool = False,
+                    val_perc: float = 0.2,
+                    skip_zero: bool = False,
                     debug_show: bool = False,
                     filename_template: str = "tile_%05d.png"):
     logger = logging.getLogger(_LOGGER_NAME)
 
-    # scan dataset sub-directories
-    src_dirs = glob(os.path.join(src_root, "*/"))
+    val_perc = np.clip(val_perc, 0.0, 1.0)
+
+    # scan dataset directories
+    src_dirs = glob(src_path)
     for src_dir in src_dirs:
         logger.info(f"Processing '{src_dir}' ...")
 
-        img_path = glob(os.path.join(src_dir, "img", "*.png"))
-        cloud_path = glob(os.path.join(src_dir, "mask_cloud", "*.png"))
-        fog_path = glob(os.path.join(src_dir, "mask_fog", "*.png"))
-        assert len(img_path) == len(cloud_path) == len(fog_path) == 1, "Number of images and corresponding labels must be equal to 1"
+        # scan files
+        img_path = glob(os.path.join(src_dir, "img", "*.*"))
+        cloud_path = glob(os.path.join(src_dir, "mask_cloud", "*.*"))
+        fog_path = glob(os.path.join(src_dir, "mask_fog", "*.*"))
+
+        files_valid = len(img_path) == len(cloud_path) == len(fog_path) == 1
+        if not files_valid:
+            logger.warning(f"Source files invalid. Skip it.")
+            continue
 
         # image
         img = cv.imread(img_path[0], cv.IMREAD_COLOR)
@@ -68,22 +76,27 @@ def prepare_dataset(src_root: str,
         assert img_tiles.shape[:2] == label_tiles.shape[:2] == roi_tiles.shape[:2]
 
         # save to disk
-        dst_subdir = os.path.join(dst_root, os.path.basename(os.path.normpath(src_dir)))
-
-        shutil.rmtree(dst_subdir, True)
-        dst_img_dir = os.path.join(dst_subdir, "img")
-        dst_label_dir = os.path.join(dst_subdir, "label")
-        dst_roi_dir = os.path.join(dst_subdir, "roi")
+        dst_train_subdir = os.path.join(dst_root, "train", os.path.basename(os.path.normpath(src_dir)))
+        shutil.rmtree(dst_train_subdir, True)
+        dst_val_subdir = os.path.join(dst_root, "val", os.path.basename(os.path.normpath(src_dir)))
+        shutil.rmtree(dst_val_subdir, True)
 
         saved_tile_cnt = 0
         for tile_idx, (img_tile, label_tile, roi_tile) in enumerate(zip(img_tiles, label_tiles, roi_tiles)):
-            if skip_empty and np.count_nonzero(label_tile) == 0:
+            if skip_zero and np.count_nonzero(label_tile) == 0:
                 continue
 
+            dst_subdir = dst_val_subdir if np.random.choice(2, p=[1 - val_perc, val_perc]) else dst_train_subdir
+
+            dst_img_dir = os.path.join(dst_subdir, "img")
             os.makedirs(dst_img_dir, exist_ok=True)
             cv.imwrite(os.path.join(dst_img_dir, filename_template % tile_idx), img_tile)
+
+            dst_label_dir = os.path.join(dst_subdir, "label")
             os.makedirs(dst_label_dir, exist_ok=True)
             cv.imwrite(os.path.join(dst_label_dir, filename_template % tile_idx), label_tile)
+
+            dst_roi_dir = os.path.join(dst_subdir, "roi")
             os.makedirs(dst_roi_dir, exist_ok=True)
             cv.imwrite(os.path.join(dst_roi_dir, filename_template % tile_idx), roi_tile)
             saved_tile_cnt += 1
@@ -109,16 +122,18 @@ def str2bool(s):
 def main():
     # create argument parser
     parser = argparse.ArgumentParser(description='Dataset Preparing For Cloud Detection In Satellite Images')
-    parser.add_argument('--src-root', dest='src_root', type=str, default="/media/yaroslav/SSD/khutornoy/data/clouds/sources/0001_0001_35146_1_35122_03_ORT_10_00",
-                        help="Path to dataset root directory")
-    parser.add_argument('--dst-root', dest='dst_root', type=str, default="/media/yaroslav/SSD/khutornoy/data/clouds/outputs/0001_0001_35146_1_35122_03_ORT_10_00",
+    parser.add_argument('--src-path', dest='src_path', type=str, default="/media/yaroslav/SSD/khutornoy/data/clouds/sources/*/*/",
+                        help="Pattern-like path to dataset directories")
+    parser.add_argument('--dst-root', dest='dst_root', type=str, default="/media/yaroslav/SSD/khutornoy/data/clouds/outputs/t256_rf1_se",
                         help="Path where to save result dataset")
-    parser.add_argument('--tile-size', dest='tile_size', type=int, default=512,
+    parser.add_argument('--tile-size', dest='tile_size', type=int, default=256,
                         help="Size of output frames")
     parser.add_argument('--resize-factor', dest='resize_factor', type=float, default=1.0,
                         help="Factor for pre-resizing frames")
-    parser.add_argument('--skip-empty', dest='skip_empty', default=True, type=str2bool,
-                        help="Whether to skip tiles without clouds or not")
+    parser.add_argument('--val-perc', dest='val_perc', type=float, default=0.2,
+                        help="Size of validation data as a percentage of total size")
+    parser.add_argument('--skip-zero', dest='skip_zero', default=True, type=str2bool,
+                        help="Whether to skip tiles without class indexes > 0")
     parser.add_argument('--debug-show', dest='debug_show', default=False, type=str2bool,
                         help="Whether to show resulting tiles or not")
     args = parser.parse_args()
@@ -128,7 +143,7 @@ def main():
     logger.info(args)
 
     # prepare dataset
-    prepare_dataset(args.src_root, args.dst_root, args.tile_size, args.resize_factor, args.skip_empty, args.debug_show)
+    prepare_dataset(args.src_path, args.dst_root, args.tile_size, args.resize_factor, args.val_perc, args.skip_zero, args.debug_show)
 
 
 if __name__ == "__main__":
