Fix transforms (normalizing, to_tensor)

core/data/datasets/cloud_dataset.py
@@ -2,7 +2,6 @@ import os
 import cv2 as cv
 import numpy as np
 from glob import glob
-from typing import Tuple
 from torch.utils.data import Dataset
 from core.data.transforms.transforms import (
     Clip,
@@ -12,7 +11,6 @@ from core.data.transforms.transforms import (
     RandomCrop,
     FromTensor,
     Denormalize,
-    RandomGamma,
     RandomMirror,
     RandomRotate,
     ConvertColor,
@@ -24,12 +22,14 @@ from core.data.transforms.transforms import (
 
 class CloudDataset(Dataset):
     def __init__(self, cfg, root_dir: str, is_train: bool):
+        self.cfg = cfg
         self.root_dir = root_dir
+        self.num_classes = len(cfg.DATASET.CLASS_LABELS)
         self.imgs = sorted(glob(os.path.join(root_dir, "*", "img", "*")))
         self.rois = sorted(glob(os.path.join(root_dir, "*", "roi", "*")))
         self.labels = sorted(glob(os.path.join(root_dir, "*", "label", "*")))
         assert len(self.imgs) == len(self.labels) == len(self.rois)
-        self.transforms = self.build_transforms(img_size=cfg.INPUT.IMAGE_SIZE, is_train=is_train)
+        self.transforms = self.build_transforms(is_train=is_train)
 
     def __len__(self):
         return len(self.imgs)
@@ -45,43 +45,47 @@ class CloudDataset(Dataset):
 
         return image, label, roi
 
-    def build_transforms(self, img_size: Tuple[int, int], is_train: bool = True):
+    def build_transforms(self, is_train: bool = True):
+        if self.cfg.INPUT.DEPTH == 1:
+            transform = [ConvertColor('BGR', 'GRAY')]
+        else:
+            transform = [ConvertColor('BGR', 'RGB')]
+
         if is_train:
-            transform = [
-                ConvertColor('BGR', 'GRAY'),
+            transform += [
                 RandomCrop(0.5, 1.0, 0.5, True),
                 RandomMirror(0.5, 0.5),
                 RandomRotate(-45, 45, 0.5),
-                Resize(img_size),
+                Resize(self.cfg.INPUT.IMAGE_SIZE),
                 ConvertFromInts(),
-                # RandomGamma(0.5, 2.0, 0.25),
                 Clip()
             ]
         else:
-            transform = [
-                ConvertColor('BGR', 'GRAY'),
-                Resize(img_size),
+            transform += [
+                Resize(self.cfg.INPUT.IMAGE_SIZE),
                 ConvertFromInts(),
                 Clip()
             ]
 
-        transform = transform + [Normalize(norm_roi=True), ToTensor()]
+        transform = transform + [Normalize(self.cfg.INPUT.NORM_MEAN, self.cfg.INPUT.NORM_SCALE),
+                                 ToTensor()]
 
         return TransformCompose(transform)
 
     def visualize(self, tick_ms: int = 25):
         back_transforms = [
             FromTensor(),
-            Denormalize(denorm_roi=True),
+            Denormalize(self.cfg.INPUT.NORM_MEAN, self.cfg.INPUT.NORM_SCALE),
             ConvertToInts()
         ]
         back_transforms = TransformCompose(back_transforms)
 
+        color_step = int(255 / (self.num_classes - 1))
         for idx in range(0, self.__len__()):
             input, label, roi = self.__getitem__(idx)
             input, label, roi = back_transforms(input, label, roi)
 
-            label = 125 * np.concatenate(input.shape[-1] * [label], -1) # TODO: 125
+            label = color_step * np.concatenate(input.shape[-1] * [label], -1)
             roi = np.concatenate(input.shape[-1] * [roi], -1)
             collage = np.concatenate([input, label, roi], axis=1)
 

core/data/transforms/transforms.py
@@ -48,25 +48,22 @@ class Clip(object):
 
 
 class Normalize(object):
-    def __init__(self, norm_roi: bool = True):
-        self.norm_roi = norm_roi
+    def __init__(self, mean = [0], scale = [255]):
+        self.mean = mean
+        self.scale = scale
 
     def __call__(self, input, target, roi = None):
-        input = input.astype(np.float32) / 255.0
-        if roi is not None and self.norm_roi:
-            roi = roi.astype(np.float32) / 255.0
+        input = (input.astype(np.float32) - self.mean) / self.scale
 
         return input, target, roi
 
-
 class Denormalize(object):
-    def __init__(self, denorm_roi: bool = True):
-        self.denorm_roi = denorm_roi
+    def __init__(self, mean = [0], scale = [255]):
+        self.mean = mean
+        self.scale = scale
 
     def __call__(self, input, target, roi = None):
-        input = input.astype(np.float32) * 255.0
-        if roi is not None and self.denorm_roi:
-            roi = roi.astype(np.float32) * 255.0
+        input = self.scale * input + self.mean
 
         return input, target, roi
 
@@ -83,9 +80,10 @@ class ToTensor:
 
         # to tensor
         input = torch.from_numpy(input.astype(np.float32)).permute(2, 0, 1)
-        target = torch.from_numpy(target.astype(np.float32)).permute(2, 0, 1)
+        target = torch.from_numpy(target.astype(np.int64)).permute(2, 0, 1)
         if roi is not None:
             roi = torch.from_numpy(roi.astype(np.float32)).permute(2, 0, 1)
+            roi = roi / 255.0
 
         return input, target, roi
 
@@ -98,6 +96,7 @@ class FromTensor:
         input = input.permute(1, 2, 0).numpy().astype(self.dtype)
         target = target.permute(1, 2, 0).numpy().astype(self.dtype)
         if roi is not None:
+            roi = 255.0 * roi
             roi = roi.permute(1, 2, 0).numpy().astype(self.dtype)
 
         return input, target, roi
@@ -233,6 +232,6 @@ class RandomGamma(object):
         if np.random.choice([0, 1], size=1, p=[1-self.probability, self.probability]):
             gamma = np.random.uniform(self.lower, self.upper)
             # if np.mean(input) > 100:
-            input = pow(input / 255., gamma) * 255.
+            input = pow(input / 255., gamma) * 255. # TODO: check it
 
         return input, target, roi

core/engine/train.py
@@ -3,10 +3,11 @@ import torch
 import logging
 import numpy as np
 from tqdm import tqdm
+from core.utils.colors import get_rgb_colors
 from core.engine.validation import do_validation
 from torch.utils.tensorboard import SummaryWriter
-from core.engine.metrics import Dice, JaccardIndex, Precision, Recall
 from core.utils.tensorboard import update_tensorboard_image_samples
+from core.engine.metrics import Dice, JaccardIndex, Precision, Recall
 
 
 def update_summary_writer(summary_writer, stats, iterations, optimizer, global_step, class_labels, is_train: bool = True):
@@ -80,10 +81,11 @@ def do_train(cfg,
 
     # create metrics
     cross_entropy = torch.nn.CrossEntropyLoss(reduction='none')
-    dice_metric = Dice(len(cfg.MODEL.CLASS_LABELS))
-    jaccard_metric = JaccardIndex(len(cfg.MODEL.CLASS_LABELS))
-    precision_metric = Precision(len(cfg.MODEL.CLASS_LABELS))
-    recall_metric = Recall(len(cfg.MODEL.CLASS_LABELS))
+    num_classes = len(cfg.DATASET.CLASS_LABELS)
+    dice_metric = Dice(num_classes)
+    jaccard_metric = JaccardIndex(num_classes)
+    precision_metric = Precision(num_classes)
+    recall_metric = Recall(num_classes)
 
     # epoch loop
     for epoch in range(start_epoch, end_epoch):
@@ -111,9 +113,9 @@ def do_train(cfg,
 
             # get data
             input, target_labels, roi = data_entry
-            input = input.to(device)                                                # (n, c, h, w)
-            target_labels = target_labels.squeeze(1).type(torch.long).to(device)    # (n, h, w)
-            roi = roi.squeeze(1).to(device)                                         # (n, h, w)
+            input = input.to(device)                               # (n, c, h, w)
+            target_labels = target_labels.squeeze(1).to(device)    # (n, h, w)
+            roi = roi.squeeze(1).to(device)                        # (n, h, w)
 
             # forward model
             output = model(input) # (n, k, h, w)
@@ -150,6 +152,7 @@ def do_train(cfg,
                                              pred_labels=pred_labels, 
                                              target_labels=target_labels,
                                              min_metric_better=True,
+                                             class_colors=get_rgb_colors(num_classes, mean=cfg.INPUT.NORM_MEAN, scale=cfg.INPUT.NORM_SCALE),
                                              blending_alpha=cfg.TENSORBOARD.ALPHA_BLENDING,
                                              nonzero_factor=0.25)
 
@@ -161,6 +164,7 @@ def do_train(cfg,
                                              pred_labels=pred_labels, 
                                              target_labels=target_labels,
                                              min_metric_better=False,
+                                             class_colors=get_rgb_colors(num_classes, mean=cfg.INPUT.NORM_MEAN, scale=cfg.INPUT.NORM_SCALE),
                                              blending_alpha=cfg.TENSORBOARD.ALPHA_BLENDING,
                                              nonzero_factor=0.25)
 
@@ -226,7 +230,7 @@ def do_train(cfg,
                                       val_stats['iterations'],
                                       optimizer,
                                       global_step,
-                                      cfg.MODEL.CLASS_LABELS,
+                                      cfg.DATASET.CLASS_LABELS,
                                       False)
 
         # save epoch results
@@ -238,7 +242,7 @@ def do_train(cfg,
                                       iteration + 1,
                                       optimizer,
                                       global_step,
-                                      cfg.MODEL.CLASS_LABELS,
+                                      cfg.DATASET.CLASS_LABELS,
                                       True)
 
     # save final model

core/engine/validation.py
@@ -1,19 +1,18 @@
 import torch
-import logging
 from tqdm import tqdm
-from core.engine.metrics import Dice, JaccardIndex, Precision, Recall
+from core.utils.colors import get_rgb_colors
 from core.utils.tensorboard import update_tensorboard_image_samples
+from core.engine.metrics import Dice, JaccardIndex, Precision, Recall
 
 
 def do_validation(cfg, model, data_loader, device):
-    logger = logging.getLogger("CORE")
-
     # create metrics
     cross_entropy = torch.nn.CrossEntropyLoss(reduction='none')
-    dice_metric = Dice(len(cfg.MODEL.CLASS_LABELS))
-    jaccard_metric = JaccardIndex(len(cfg.MODEL.CLASS_LABELS))
-    precision_metric = Precision(len(cfg.MODEL.CLASS_LABELS))
-    recall_metric = Recall(len(cfg.MODEL.CLASS_LABELS))
+    num_classes = len(cfg.DATASET.CLASS_LABELS)
+    dice_metric = Dice(num_classes)
+    jaccard_metric = JaccardIndex(num_classes)
+    precision_metric = Precision(num_classes)
+    recall_metric = Recall(num_classes)
 
     # create stats
     stats = {
@@ -66,6 +65,7 @@ def do_validation(cfg, model, data_loader, device):
                                              pred_labels=pred_labels, 
                                              target_labels=target_labels,
                                              min_metric_better=True,
+                                             class_colors=get_rgb_colors(num_classes, mean=cfg.INPUT.NORM_MEAN, scale=cfg.INPUT.NORM_SCALE),
                                              blending_alpha=cfg.TENSORBOARD.ALPHA_BLENDING,
                                              nonzero_factor=0.1)
 
@@ -77,6 +77,7 @@ def do_validation(cfg, model, data_loader, device):
                                              pred_labels=pred_labels, 
                                              target_labels=target_labels,
                                              min_metric_better=False,
+                                             class_colors=get_rgb_colors(num_classes, mean=cfg.INPUT.NORM_MEAN, scale=cfg.INPUT.NORM_SCALE),
                                              blending_alpha=cfg.TENSORBOARD.ALPHA_BLENDING,
                                              nonzero_factor=0.1)
 

test.py
@@ -1,18 +1,17 @@
 import os
 import torch
-import shutil
-import logging
 import argparse
 import cv2 as cv
 import numpy as np
-from torch import nn
+from tqdm import tqdm
 from glob import glob
 from core.config import cfg as cfg
 from core.models import build_model
 from core.utils.logger import setup_logger
-from core.data.transforms.functional import image_to_tiles, tiles_to_image
-from core.utils.checkpoint import CheckPointer
+from core.utils.colors import get_rgb_colors
 from core.utils.tensorboard import draw_labels
+from core.utils.checkpoint import CheckPointer
+from core.data.transforms.functional import image_to_tiles, tiles_to_image
 
 
 _LOGGER_NAME = "MODEL TEST"
@@ -20,7 +19,7 @@ _LOGGER_NAME = "MODEL TEST"
 
 def test_model(cfg,
                src_path: str,
-               dst_path: str,
+               dst_root: str,
                tile_size: int,
                resize_factor: float):
     # create device
@@ -35,56 +34,58 @@ def test_model(cfg,
     checkpointer = CheckPointer(model, None, None, cfg.OUTPUT_DIR)
     checkpointer.load(cfg.MODEL.PRETRAINED_WEIGHTS)
 
-    # read image
-    image = cv.imread(src_path, cv.IMREAD_COLOR)
+    # create label colors
+    num_classes = len(cfg.DATASET.CLASS_LABELS)
+    colors_rgb = get_rgb_colors(num_classes, mean=cfg.INPUT.NORM_MEAN, scale=cfg.INPUT.NORM_SCALE)
 
-    # resize image
-    if resize_factor != 1.0:
-        image = cv.resize(image, dsize=None, fx=resize_factor, fy=resize_factor, interpolation=cv.INTER_AREA)
+    # scan sources
+    src_files = sorted(glob(src_path))
+    for src_file in tqdm(src_files):
+        # read image
+        image = cv.imread(src_file, cv.IMREAD_COLOR)
 
-    # split to tiles
-    tiles_pad = 256
-    tiles = image_to_tiles(image, tile_size, tiles_pad)
+        # resize image
+        if resize_factor != 1.0:
+            image = cv.resize(image, dsize=None, fx=resize_factor, fy=resize_factor, interpolation=cv.INTER_AREA)
 
-    # TODO: as parameter
-    class_colors = [
-        (0, 0, 0), # background
-        (1, 0, 0.5), # fog
-        (0, 0.5, 1), # cloud
-    ]
+        if cfg.INPUT.DEPTH == 1:
+            image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+            image = np.expand_dims(image, axis=-1)
 
-    # process tiles
-    result_tiles = []
-    with torch.no_grad():
-        for tile in tiles:
-            # prepare input
-            input = torch.from_numpy(tile).type(torch.float).to(device) # TODO: check input depth
-            input = input / 255
-            input = input.permute(2, 0, 1) # (C, H, W)
+        # split to tiles
+        tiles_pad = 0
+        tiles = image_to_tiles(image, tile_size, tiles_pad)
 
-            # do prediction
-            output = model.forward(input.unsqueeze(0)) # (1, K, H, W)
-            output = output.squeeze(0) # (K, H, W)
-            label = torch.softmax(output, dim=0).argmax(dim=0) # (H, W)
+        # process tiles
+        result_tiles = []
+        with torch.no_grad():
+            for tile in tiles:
+                # prepare input
+                tile = (tile - cfg.INPUT.NORM_MEAN) / cfg.INPUT.NORM_SCALE
+                input = torch.from_numpy(tile).type(torch.float).to(device)
+                input = input.permute(2, 0, 1) # (c, h, w)
 
-            # draw labels
-            result = draw_labels(input, label, class_colors, 0.15)
+                # do prediction
+                output = model.forward(input.unsqueeze(0))          # (1, k, h, w)
+                output = output.squeeze(0)                          # (k, h, w)
+                label = torch.softmax(output, dim=0).argmax(dim=0)  # (h, w)
 
-            # to numpy
-            result = result.permute(1, 2, 0).cpu().numpy()
-            result = (255 * result).astype(np.uint8)
-            result_tiles.append(result)
+                # draw labels
+                result = draw_labels(input, label, colors_rgb, 0.25)
 
-            # # debug show
-            # cv.imshow('tile', tile)
-            # cv.imshow('result', result)
-            # if cv.waitKey(0) & 0xFF == ord('q'):
-            #     break
+                # to numpy
+                result = result.permute(1, 2, 0).cpu().numpy()
+                result = result * cfg.INPUT.NORM_SCALE + cfg.INPUT.NORM_MEAN
+                result = result.astype(np.uint8)
+                result_tiles.append(result)
 
-    # result tiles to image
-    result_tiles = np.stack(result_tiles, axis=0)
-    result_image = tiles_to_image(result_tiles, image.shape[0], image.shape[1], tiles_pad)
-    cv.imwrite(dst_path, result_image)
+        # result tiles to image
+        os.makedirs(dst_root, exist_ok=True)
+        dst_file = os.path.join(dst_root, os.path.basename(os.path.normpath(src_file)))
+
+        result_tiles = np.stack(result_tiles, axis=0)
+        result_image = tiles_to_image(result_tiles, image.shape[0], image.shape[1], tiles_pad)
+        cv.imwrite(dst_file, result_image)
             
 
 def main():
@@ -92,11 +93,10 @@ def main():
     parser = argparse.ArgumentParser(description='Cloud Detection In Satellite Images Model Testing With PyTorch')
     parser.add_argument("--config-file", dest="config_file", required=False, type=str, default="configs/cfg.yaml",
                         help="Path to config file")
-    # TODO: src-path (pattern)
-    parser.add_argument('--src-path', dest="src_path", required=False, type=str, default="data/tests/0001_0001_34715_1_34690_01_ORT_10_01_09.png",
-                        help='Path to source image to segment')
-    parser.add_argument('--dst-path', dest="dst_path", required=False, type=str, default="data/results/0001_0001_34715_1_34690_01_ORT_10_01_09.png",
-                        help='Path where to save segmentation result')
+    parser.add_argument('--src-path', dest="src_path", required=False, type=str, default="data/tests/*",
+                        help='Pattern-like path to source images to segment')
+    parser.add_argument('--dst-root', dest="dst_root", required=False, type=str, default="data/results",
+                        help='Path where to save results')
     parser.add_argument('--tile-size', dest='tile_size', type=int, default=1536,
                         help="Size of output frames")
     parser.add_argument('--resize-factor', dest='resize_factor', type=float, default=1.0,
@@ -115,7 +115,7 @@ def main():
     cfg.freeze()
 
     # test model
-    test_model(cfg, args.src_path, args.dst_path, args.tile_size, args.resize_factor)
+    test_model(cfg, args.src_path, args.dst_root, args.tile_size, args.resize_factor)
 
 
 if __name__ == "__main__":